60a6cfcbe60b1af9a4d7c7c73e2d28f54b7fe539
Simon Elliott <simon.elliott@hashicorp.com>
2023-04-13 15:43:01 +0100

    develop - removed split function

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "vpc" {
   name                 = var.vpc_name
   env                  = var.env_name
   vpc_cidr             = var.vpc_cidr
-  public_subnet_cidr   = split(", ", var.public_subnet_cidr)
-  private_subnet_cidr  = split(", ", var.private_subnet_cidr)
-  database_subnet_cidr = split(", ", var.database_subnet_cidr)
+  public_subnet_cidr   = var.public_subnet_cidr
+  private_subnet_cidr  = var.private_subnet_cidr
+  database_subnet_cidr = var.database_subnet_cidr
 
 }
