f660351613cf34a1674868ebbdc2ae38a860405e
Simon Elliott <simon.elliott@hashicorp.com>
2023-04-24 14:08:05 +0100

    develop - added addiotnal infrastructure components

--- main.tf
+++ main.tf
@@ -7,22 +7,17 @@ module "vpc" {
   public_subnet_cidr   = split(", ", var.public_subnet_cidr)
   private_subnet_cidr  = split(", ", var.private_subnet_cidr)
   database_subnet_cidr = split(", ", var.database_subnet_cidr)
-
 }
-
-
 module "internet-gateway" {
-  source = "/Users/simon.elliott/Documents/Code/Terraform/modules/tfc/aws-tf-module-igw"
-  //source               = "app.terraform.io/HashiCorp_TFC_Automation_Demo/module-internet-gateway/module"
-  //version              = "1.0.0"
+  source   = "app.terraform.io/HashiCorp_TFC_Automation_Demo/module-internet-gateway/module"
+  version  = "1.0.0"
   vpc_id   = module.vpc.vpc_id
   name     = var.vpc_name
   env_name = var.env_name
-
 }
-
 module "public-route" {
-  source     = "git@github.com:sce81/aws-tf-module-route-table.git"
+  source     = "app.terraform.io/HashiCorp_TFC_Automation_Demo/module-route-table/module"
+  version    = "1.0.0"
   route_name = ["public-route-table"]
   vpc_id     = module.vpc.vpc_id
   name       = var.vpc_name
@@ -30,11 +25,21 @@ module "public-route" {
   subnet_ids = module.vpc.public_subnet_ids
   route_info = local.public_route_info
 }
-
 module "nat_gateway" {
-  source            = "git@github.com:sce81/terraform-aws-module-nat-gateway"
+  source            = "app.terraform.io/HashiCorp_TFC_Automation_Demo/module-nat-gateway/module"
+  version           = "1.0.0"
   nat_gateway_count = 1
   name              = var.vpc_name
   env_name          = var.env_name
   vpc_id            = module.vpc.vpc_id
+}
+module "private-route" {
+  source     = "app.terraform.io/HashiCorp_TFC_Automation_Demo/module-route-table/module"
+  version    = "1.0.0"
+  route_name = ["private-route-table"]
+  vpc_id     = module.vpc.vpc_id
+  name       = var.vpc_name
+  env_name   = var.env_name
+  subnet_ids = [module.vpc.private_subnet_ids]
+  route_info = local.private_route_info
 }
