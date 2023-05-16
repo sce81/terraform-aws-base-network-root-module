9d558eb69c68557682cae7c5e0c3049b52101261
Simon Elliott <simon.elliott@hashicorp.com>
2023-05-16 10:20:59 +0100

    develop - renamed initialise.tf to provisioners.tf

---
+++ provisioners.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "eu-west-1"
+
+  //  assume_role {
+  //    role_arn    = var.role_arn
+  //    external_id = "terraform_runner"
+  //  }
+  default_tags {
+    tags = {
+      env     = var.env_name
+      project = var.project_name
+    }
+  }
+}
+
+terraform {
+  cloud {
+    organization = "HashiCorp_TFC_Automation_Demo"
+
+    workspaces {
+      name = "aws_workspace_base_infra-dev"
+    }
+  }
+  required_version = "~> 1.4.0"
+
+  required_providers {
+    aws = {
+      version = "4.62.0"
+      source  = "hashicorp/aws"
+    }
+  }
+}
