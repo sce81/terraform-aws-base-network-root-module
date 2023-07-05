0cbbd56dac29b9a847150ef9b643d9618d68127f
Simon Elliott <simon.elliott@hashicorp.com>
2023-07-05 12:16:03 +0100

    develop - bumped route version tag

--- data.tf
+++ data.tf
@@ -7,8 +7,6 @@ locals {
   private_route_info = [
     {
       route_cidr     = "0.0.0.0/0"
-      nat_gateway_id = element(flatten(module.nat_gateway.*.id[*]), 0)
+      nat_gateway_id = module.nat_gateway.id[0]
   }]
-
-
 }
