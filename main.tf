ec5ac69fae11d83cdfc692f73f4edaa0ec5c64f7
Simon Elliott <simon.elliott@hashicorp.com>
2023-07-14 12:41:57 +0100

    develop - cleanup

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 module "vpc" {
   source               = "app.terraform.io/HashiCorp_TFC_Automation_Demo/vpc-network/aws"
-  version              = "1.0.2"
-  //source = "/Users/simon.elliott/Documents/Code/Terraform/modules/aws/terraform-module-aws-vpc"
-  
+  version              = "1.0.2"  
   name                 = var.vpc_name
   env                  = var.env_name
   vpc_cidr             = var.vpc_cidr
