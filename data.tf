3c146e001e0c4440f2a0b763c6f221e08c6343ce
Simon Elliott <simon.elliott@hashicorp.com>
2023-07-19 13:12:06 +0100

    develop - experimenting with dynamic routes & disabling nat gws

--- data.tf
+++ data.tf
@@ -1,12 +1,19 @@
 locals {
-  public_route_info = [
+
+
+  public_route_info  = var.enable_igw == false ? var.public_route_info : local.public_route_igw
+  private_route_info = var.enable_natgw == false ? var.private_route_info : local.private_route_natgw
+
+  public_route_igw = [
     {
       route_cidr = "0.0.0.0/0"
-      gateway_id = module.internet-gateway.igw
-  }]
-  private_route_info = [
+      gateway_id = var.enable_igw == false ? null : module.internet-gateway[0].igw
+    }
+  ]
+  private_route_natgw = [
     {
       route_cidr     = "0.0.0.0/0"
-      nat_gateway_id = module.nat_gateway.id[0]
-  }]
+      nat_gateway_id = var.enable_natgw == false ? null : module.nat_gateway[0].id
+    }
+  ]
 }
