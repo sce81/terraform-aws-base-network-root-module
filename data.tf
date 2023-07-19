acd6b18868c0f921f40ca34380b9524e18ef344a
Simon Elliott <simon.elliott@hashicorp.com>
2023-07-19 14:53:40 +0100

    develop - enabling nat/igw based on vars

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
 locals {
 
 
-  public_route_info  = var.enable_igw == false ? tolist(object(var.public_route_info)) : local.public_route_igw
-  private_route_info = var.enable_natgw == false ? tolist(object(var.private_route_info)) : local.private_route_natgw
+  public_route_info  = var.enable_igw == false ? tolist(var.public_route_info) : local.public_route_igw
+  private_route_info = var.enable_natgw == false ? tolist(var.private_route_info) : local.private_route_natgw
 
   public_route_igw = [
     {
